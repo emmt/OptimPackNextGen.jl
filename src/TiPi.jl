0f0e5a54a6539c987b6fcece63731edc66ec957c
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2015-03-07 10:15:59 +0100

    Add differentiable cost functions

--- src/TiPi.jl
+++ src/TiPi.jl
@@ -1,5 +1,29 @@
+#
+# TiPi.jl --
+#
+# Toolkit for Inverse Problems and Imaging in Julia.
+#
+#------------------------------------------------------------------------------
+#
+# This file is part of TiPi.jl licensed under the MIT "Expat" License.
+#
+# Copyright (C) 2015, Éric Thiébaut & Jonathan Léger.
+#
+#------------------------------------------------------------------------------
+
 module TiPi
 
-# package code goes here
+export cost, CostParam
+
+include("cost.jl")
 
 end # module
+
+# Local Variables:
+# mode: Julia
+# tab-width: 8
+# indent-tabs-mode: nil
+# fill-column: 79
+# coding: utf-8
+# ispell-local-dictionary: "american"
+# End:
