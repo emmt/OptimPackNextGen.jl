ffdaa33c9d44704a9fdf552324965c8cf6d29544
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2016-04-19 18:15:30 +0200

    Add tests for kernels

---
+++ test/kernels-tests.jl
@@ -0,0 +1,43 @@
+module TiPiTests
+
+const PLOTTING = true
+if PLOTTING
+    using PyCall; pygui(:gtk); # can be: :wx, :gtk, :qt
+    using LaTeXStrings
+    import PyPlot; const plt = PyPlot
+end
+include("../src/kernels.jl")
+
+function runtests()
+    for (name, ker) in (("Box", Kernels.box),
+                        ("Box (type)", Kernels.BoxKernel{Float32}),
+                        ("Catmull-Rom",  Kernels.catmull_rom),
+                        ("Mitchell-Netravali",  Kernels.mitchell_netravili))
+        println(name," kernel:")
+        println(" - support: ", length(ker))
+        println(" - normalized: ", Kernels.isnormalized(ker))
+        println(" - cardinal: ", Kernels.iscardinal(ker))
+        println(" - ker(0): ", ker(0)) # test conversion
+    end
+
+    plt.figure(1)
+    plt.clf()
+    x = linspace(-5,5,1000);
+    plt.plot(x, Kernels.box(x), color="darkgreen",
+             linewidth=2.0, linestyle="-");
+    plt.plot(x, Kernels.triangle(x), color="darkblue",
+             linewidth=2.0, linestyle="-");
+    plt.plot(x, Kernels.quadratic(x), color="darkcyan",
+             linewidth=2.0, linestyle="-");
+    plt.plot(x, Kernels.cubic(x), color="firebrick",
+             linewidth=2.0, linestyle="-");
+    plt.plot(x, Kernels.catmull_rom(x), color="orange",
+             linewidth=2.0, linestyle="-");
+    plt.plot(x, Kernels.mitchell_netravili(x), color="violet",
+             linewidth=2.0, linestyle="-");
+    plt.title("Some kernel functions");
+end
+
+runtests()
+
+end # module
