8a253e2976d86b0a7a5d951aa34b5921aa60c346
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2016-07-30 00:53:51 +0200

    Add fit method

--- src/interp.jl
+++ src/interp.jl
@@ -33,12 +33,12 @@ function rows(op::Interpolator)
     @assert length(op.A) == width*nrows
     @assert length(op.J) == width*nrows
     I = Array(Int, width*nrows)
-    k0 = 0
+    K = 1:width
     for i in 1:nrows
-        for k in k0+1:k0+width
+        for k in K
             @inbounds I[k] = i
         end
-        k0 += width
+        K += width
     end
     return I
 end
@@ -168,6 +168,7 @@ function apply_adjoint!{T,N}(dst::AbstractArray{T,1},
     end
 end
 
+# Convert to a sparse matrix.
 function sparse(op::Interpolator)
     const nrows = op.nrows
     const ncols = op.ncols
@@ -184,3 +185,205 @@ function sparse(op::Interpolator)
     end
     sparse(I, op.J, op.A, nrows, ncols)
 end
+
+function AtWA{T,N}(op::Interpolator{T,N}, wgt::AbstractArray{T,N})
+    ncols = op.ncols
+    AtWA!(Array(T, ncols, ncols), op, wgt)
+end
+
+function AtA{T,N}(op::Interpolator{T,N})
+    ncols = op.ncols
+    AtA!(Array(T, ncols, ncols), op)
+end
+
+function AtA!{T,N}(dst::AbstractArray{T,2}, op::Interpolator{T,N})
+    const nrows = op.nrows
+    const ncols = op.ncols
+    const width = op.width
+    @assert size(dst) == (ncols, ncols)
+    fill!(dst, zero(T))
+    A::Vector{T} = coefficients(op)
+    J::Vector{Int} = columns(op)
+    K = 1:width
+    @assert length(J) == length(A)
+    @inbounds begin
+        for i in 1:nrows
+            for k in K
+                1 ≤ J[k] ≤ ncols || error("corrupted interpolator table")
+            end
+            for k1 in K
+                j1 = J[k1]
+                fact = A[k1]
+                for k2 in K
+                    j2 = J[k2]
+                    dst[j1,j2] += A[k2]*fact
+                end
+            end
+            K += width
+        end
+    end
+
+    return dst
+end
+
+function AtWA!{T,N}(dst::AbstractArray{T,2}, op::Interpolator{T,N},
+                    wgt::AbstractArray{T,N})
+    const nrows = op.nrows
+    const ncols = op.ncols
+    const width = op.width
+    @assert size(dst) == (ncols, ncols)
+    @assert size(wgt) == output_size(op)
+    fill!(dst, zero(T))
+    A::Vector{T} = coefficients(op)
+    J::Vector{Int} = columns(op)
+    K = 1:width
+    @assert length(J) == length(A)
+    @inbounds begin
+        for i in 1:nrows
+            for k in K
+                1 ≤ J[k] ≤ ncols || error("corrupted interpolator table")
+            end
+            wgt_i = wgt[i]
+            for k1 in K
+                j1 = J[k1]
+                fact = wgt_i*A[k1]
+                for k2 in K
+                    j2 = J[k2]
+                    dst[j1,j2] += A[k2]*fact
+                end
+            end
+            K += width
+        end
+    end
+    return dst
+end
+
+# Default regularization levels.
+const RGL_EPS = 1e-9
+const RGL_MU = 0.0
+
+doc"""
+
+    fit(A, y[, w][; epsilon=1e-9, mu=0.0]) -> x
+
+performs a linear fit of `y` by the model `A*x`.  The returned value `x`
+minimizes:
+
+    (A*x - y)'*diag(w)*(A*x - y)
+
+where `w` are some weights.  If `w` is not specified, all weights are assumed
+to be equal to one; otherwise `w` must be an array of nonnegative values and of
+same size as `y`.
+
+Keywords `epsilon` and `mu` may be specified to regularize the solution and
+minimize:
+
+    (A*x - y)'*diag(w)*(A*x - y) + rho*(epsilon*norm2(x) + mu*norm2(D*x))
+
+where `D` is a finite difference operator and `rho` is the maximum diagonal
+element of `A'*diag(w)*A`.
+
+"""
+function fit{T,N}(A::Interpolator{T,N}, y::AbstractArray{T,N};
+                  epsilon::Real = RGL_EPS, mu::Real = RGL_MU)
+    @assert size(y) == output_size(A)
+    @assert size(w) == size(y)
+
+    # Compute RHS vector A'*W*y with W = diag(w).
+    rhs = A'*(w.*y)
+
+    # Compute LHS matrix A'*W*A with W = diag(w).
+    lhs = AtWA(A, w)
+
+    # Regularize a bit.
+    regularize!(lhs, epsilon, mu)
+
+    # Solve the linear equations.
+    cholfact!(lhs,:U,Val{true})\rhs
+end
+
+function fit{T,N}(A::Interpolator{T,N}, y::AbstractArray{T,N};
+                  epsilon::Real = RGL_EPS, mu::Real = RGL_MU)
+    @assert size(y) == output_size(A)
+    @assert size(w) == size(y)
+
+    # Compute RHS vector A'*y.
+    rhs = A'*y
+
+    # Compute LHS matrix A'*W*A with W = diag(w).
+    lhs = AtA(A)
+
+    # Regularize a bit.
+    regularize!(lhs, epsilon, mu)
+
+    # Solve the linear equations.
+    cholfact!(lhs,:U,Val{true})\rhs
+end
+
+doc"""
+    regularize(A, ϵ, μ) -> R
+
+regularizes the symmetric matrix `A` to produce the matrix:
+
+    R = A + ρ(ϵ I + μ D'*D)
+
+where `I` is the identity, `D` is a finite difference operator and `ρ` is the
+maximum diagonal element of `A`.  The in-place version:
+
+    regularize!(A, ϵ, μ) -> A
+
+stores the regularized matrix in `A` (and returns it).
+
+"""
+function regularize end
+
+regularize{T<:AbstractFloat}(A::AbstractArray{T,2}, args...) =
+    regularize!(copy!(Array(T, size(A)), A), args...)
+
+function regularize!{T<:AbstractFloat}(A::AbstractArray{T,2},
+                                       eps::Real = RGL_EPS,
+                                       mu::Real = RGL_MU)
+    regularize!(A, T(eps), T(mu))
+end
+
+function regularize!{T<:AbstractFloat}(A::AbstractArray{T,2}, eps::T, mu::T)
+    local rho::T
+    @assert eps ≥ zero(T)
+    @assert mu ≥ zero(T)
+    @assert size(A,1) == size(A,2)
+    const n = size(A,1)
+    if eps > zero(T) || mu > zero(T)
+        rho = A[1,1]
+        for j in 2:n
+            d = A[j,j]
+            rho = max(rho, d)
+        end
+        rho > zero(T) || error("we have a problem!")
+    end
+    if eps > zero(T)
+        q = eps*rho
+        for j in 1:n
+            A[j,j] += q
+        end
+    end
+    if mu > zero(T)
+        q = rho*mu
+        if n ≥ 2
+            r = q + q
+            A[1,1] += q
+            A[2,1] -= q
+            for i in 2:n-1
+                A[i-1,i] -= q
+                A[i,  i] += r
+                A[i+1,i] -= q
+            end
+            A[n-1,n] -= q
+            A[n,  n] += q
+        elseif n == 1
+            A[1,1] += q
+        end
+    end
+    return A
+end
+
+@doc @doc(regularize) regularize!
